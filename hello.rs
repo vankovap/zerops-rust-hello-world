8e9c00289c91217acb97a9395894f0c21b375417
vankovap <petravankova.pv@gmail.com>
2024-03-20 13:58:26 +0100

    Update hello.rs

--- hello.rs
+++ hello.rs
@@ -1,3 +1,13 @@
+use log::debug;
+use log::error;
+use log::info;
+use log::warn;
+
 fn main() {
+    env_logger::init();
+    debug!("Mary has a little lamb");
+    error!("{}", "Its fleece was white as snow");
+    info!("{:?}", "And every where that Mary went");
+    warn!("{:#?}", "The lamb was sure to go");
     println!("Hello World!");
 }
